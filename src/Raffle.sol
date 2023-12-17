dc169076385f7abc3c0cd3e607c158848c8061c0
Owenzo7 <owenlee545@gmail.com>
2023-12-17 14:17:29 +0300

    first commit

---
+++ src/Raffle.sol
@@ -0,0 +1,53 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+// SPDX-License-Identifer: MIT
+
+pragma solidity ^0.8.17;
+
+/**@title A sample Raffle Contract
+ * @author Owen Lee
+ * @notice This contract is for creating a sample raffle contract
+ * @dev This implements the Chainlink VRF Version 2
+ */
+contract Raffle {
+    error Raffle__NotEnoughEthSent();
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+    }
+
+    function pickWinner() public {}
+
+    // Getter functions
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
