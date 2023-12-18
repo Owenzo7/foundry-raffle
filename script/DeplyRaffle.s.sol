f415639918f2cebe0c662ccb82b7bc1d8deab0f2
Owenzo7 <owenlee545@gmail.com>
2023-12-18 20:55:03 +0300

    done with making the deploy scripts

--- script/DeplyRaffle.s.sol
+++ script/DeplyRaffle.s.sol
@@ -4,14 +4,33 @@ pragma solidity ^0.8.17;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     Raffle raffle;
 
     function run() external returns (Raffle) {
-        vm.startBroadcast();
+        HelperConfig helperconfig = new HelperConfig();
 
-        raffle = new Raffle();
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit
+        ) = helperconfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        raffle = new Raffle(
+            entranceFee,
+            interval,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
+        );
+
         vm.stopBroadcast();
 
         return raffle;
